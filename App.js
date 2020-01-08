26dd412472fd793a92c458b6195714928714c5b8
mustafcode10 <mustafdiiriye@hotmail.com>
2020-01-08 16:38:27 +0200

    fixed errors

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 
 
-<<<<<<< HEAD
 import ModulesNavigator from './navigation/ModulesNavigator';
 useScreens();
 
@@ -11,15 +10,6 @@ export default function App() {
   return (
     <ModulesNavigator />
   
-=======
-import MainNavigator from './navigation/MainNavigator';
-enableScreens();
-
-export default function App() {
-  return (
-    <MainNavigator />
-
->>>>>>> 3a69632d93384dc8593d514f2aaa7a3d42034364
   );
 }
 
--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,6 @@
 
 export default {
-<<<<<<< HEAD
     primaryColor: '#4a148c',
     accentColor: '#ff6f00',
     lightBlueColor: '#4169e1'
 };
-=======
-    blueColor: '#4169e1',
-    whiteColor: '#ffffff'
-}
-
-
->>>>>>> 3a69632d93384dc8593d514f2aaa7a3d42034364
